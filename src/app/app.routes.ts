e44db113eb8e8f5ee185b1023de903dbec0bd35a
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2024-06-03 10:37:20 -0500

    Configuración para la campaña tipo link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
+import { CampaingLinkComponent } from './pages/guest-view/campaing-link/campaing-link.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'change-password',
     component: ForgotPasswordComponent
+  },
+  {
+    path: 'discount/:code',
+    component: CampaingLinkComponent
   }
 ];
---
+++ src/app/pages/guest-view/campaing-link/campaing-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaing-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaing-link.component.html',
+  styleUrl: './campaing-link.component.css'
+})
+export class CampaingLinkComponent {
+
+}
