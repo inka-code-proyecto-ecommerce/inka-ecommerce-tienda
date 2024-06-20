d21347ddd7d03c10d8a786ffa72bf558b750c838
ytucayasi <saul.ytucayasi@gmail.com>
2024-06-20 00:20:21 -0500

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  URL_BACKEND: 'http://intiphone-api.incadevs.com/',
-  URL_SERVICE: 'http://intiphone-api.incadevs.com/api',
-  URL_FRONTEND: 'http://intiphone-tienda.incadevs.com',
+  URL_BACKEND: 'https://intiphone-api.incadevs.com/',
+  URL_SERVICE: 'https://intiphone-api.incadevs.com/api',
+  URL_FRONTEND: 'https://intiphone-tienda.incadevs.com',
   production: true,
 };
